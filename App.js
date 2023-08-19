26d3010bec9d6ac6119ea34b8dc0021686a2a099
Nicolas <nicolassantiagonavarrete.nsn@gmail.com>
2023-08-19 10:48:21 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from "react";
+import { Text, View, StyleSheet, Image, Button, Alert } from 'react-native'
+import image from './assets/blue-diamon.webp'
+const app = () =>{
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hello World!!</Text>
+      <Image source={{uri: 'https://picsum.photos/200'}} style={styles.imageex}/>
+      <Button
+      title="Press Me"
+      onPress={() => Alert.alert('Expample')}
+      />
     </View>
-  );
+  ) 
+  
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  container:{
+    flex: 1, 
+    justifyContent: 'center', 
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#292929'
   },
-});
+
+  title:{
+    fontSize: 30,
+    color: '#fff',
+  },
+  imageex: {
+    height: 200,
+    width: 200,
+    borderRadius: 100
+  },
+  imageex2: {
+    height: 200,
+    width: 300
+  }
+})
+
+export default app;
