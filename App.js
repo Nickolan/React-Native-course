2d19409a18062a47bc2016ea5532cf8613717d70
Nicolas <nicolassantiagonavarrete.nsn@gmail.com>
2023-08-21 12:39:24 -0300

    tutorial completed

--- App.js
+++ App.js
@@ -1,15 +1,53 @@
-import React from "react";
-import { Text, View, StyleSheet, Image, Button, Alert } from 'react-native'
-import image from './assets/blue-diamon.webp'
+import React, { useState } from "react";
+import { Text, View, StyleSheet, Image, Button, Alert, TouchableOpacity } from 'react-native'
+import * as ImagePicker from 'expo-image-picker'
+import * as Sharing from 'expo-sharing';
+ 
 const app = () =>{
+
+  const [imageSelected, setImageSelected] = useState(null)
+
+  let openPickanImage = async() =>{
+    let permisionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permisionResult.granted === false) {
+      alert('It is needed to authorize the media library')
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync()
+
+    if (pickerResult.canceled === true) {
+      return;
+    }
+    setImageSelected({uriImage: pickerResult.assets[0].uri})
+  }
+
+  let openSharing = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert('This device is not available to share with others')
+      return;
+    }
+
+    await Sharing.shareAsync(imageSelected.uriImage)
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World!!</Text>
-      <Image source={{uri: 'https://picsum.photos/200'}} style={styles.imageex}/>
-      <Button
-      title="Press Me"
-      onPress={() => Alert.alert('Expample')}
-      />
+      <Text style={styles.title}>Select Your Image!!</Text>
+      <TouchableOpacity
+        onPress={openPickanImage}
+      >
+      <Image source={{uri: imageSelected !== null ? imageSelected.uriImage : 'https://picsum.photos/200'}} style={styles.imageex}/>
+      </TouchableOpacity>
+      {
+        imageSelected !== null ?
+        <TouchableOpacity
+          onPress={openSharing}
+          style={styles.touchableImage}
+        >
+          <Text style={styles.buttonText}>Share Image</Text>
+        </TouchableOpacity> : null
+      }
     </View>
   ) 
   
@@ -35,6 +73,15 @@ const styles = StyleSheet.create({
   imageex2: {
     height: 200,
     width: 300
+  },
+  touchableImage:{
+    backgroundColor: '#00a3ff',
+    padding: 7,
+    marginTop: 10,
+  },
+  buttonText:{
+    color: '#fff',
+    fontSize: 20
   }
 })
 
